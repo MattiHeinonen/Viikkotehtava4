b1c061ecd4bd06e86bb8b08bd55a743f09de5924
MattiHeinonen <37863560+MattiHeinonen@users.noreply.github.com>
2018-09-25 12:21:53 +0300

    Merge branch 'b2'

--- MyApp.java
+++ MyApp.java
@@ -1,33 +1,46 @@
 public class MyApp {
 		public static void main(String[] args) {
-			Product product = new Product("Edam", 3.3, 120);   
+
+			Product product = new Product("Edam", 3.3, 120);
 			product.ChangeAmount(245);
-			System.out.println("Product value is " + product.countValue());      
-			product.printProduct();      
-	 } 
-	 
-} 
- 
- class Product { 
-			private String name;  
-			private double price;  
-			private int amount; //Amount in storage  
+
+
+			product.ChangePrice(4.1);
+
+			System.out.println("Product value is " + product.countValue());
+			product.printProduct();
+	 }
+
+}
+
+ class Product {
+			private String name;
+			private double price;
+			private int amount; //Amount in storage
 			public Product(String name, double price, int amount) {
-					this.name = name;   
-					this.price = price;   
-					this.amount = amount;  
-			}  public double countValue() {   
-					return(amount * price);  
-			}    
-			
-			public void printProduct() {   
+					this.name = name;
+					this.price = price;
+					this.amount = amount;
+			}  public double countValue() {
+					return(amount * price);
+			}
+
+			public void printProduct() {
 					System.out.printf("Product %s, price %4.1f and balance %d pcs",
-					name,price,amount);  
-			} 
-			
+					name,price,amount);
+			}
+
+
 			public void ChangeAmount(int newValue)
 			{
 				amount = newValue;
 			}
-					
-} 
+
+			public void ChangePrice(double newPrice)
+			{
+				price = newPrice;
+			}
+
+
+
+}
