b0f73f4ddcda792bc711e11b2d13f63cbb6344dd
MattiHeinonen <37863560+MattiHeinonen@users.noreply.github.com>
2018-09-25 11:42:28 +0300

    b1 commit

--- MyApp.java
+++ MyApp.java
@@ -1,6 +1,7 @@
 public class MyApp {
 		public static void main(String[] args) {
-			Product product = new Product("Edam", 3.3, 120);      
+			Product product = new Product("Edam", 3.3, 120);   
+			product.ChangeAmount(245);
 			System.out.println("Product value is " + product.countValue());      
 			product.printProduct();      
 	 } 
@@ -23,5 +24,10 @@ public class MyApp {
 					System.out.printf("Product %s, price %4.1f and balance %d pcs",
 					name,price,amount);  
 			} 
+			
+			public void ChangeAmount(int newValue)
+			{
+				amount = newValue;
+			}
 					
 } 
